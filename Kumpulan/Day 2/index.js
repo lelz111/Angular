408fcf7bb2384eebaa90a2cb353e22cc952431f2
lelz111 <slleonard86@gmail.com>
2025-06-24 09:59:31 +0700

    Studycase1

---
+++ Kumpulan/Day 2/index.js
@@ -0,0 +1,51 @@
+// let x = 'tes'
+// console.log(x)
+
+// const arr = ['tes', false, 42]
+// arr[2] 
+// console.log(arr[0]*2)
+
+let person = {
+    name: 'John',
+    age: 30,
+    isStudent:false
+}
+console.log(person.name)
+
+// ini array object
+const dataUser = [
+    {
+    name: 'John',
+    age: 30,
+    isStudent:false
+    },
+    {
+    name: 'aaaaa',
+    age: 30,
+    isStudent:false
+    }
+]
+console.log(dataUser[1].name)
+
+// ini array string
+const arrName = ['John', 'aaaaa'] 
+
+
+let x = 5
+let y = 2
+
+console.log(x **=y)
+
+let a= 2.0
+let b = '2'
+
+console.log(a == b)
+
+let age = 9
+console.log(age >=18 ? 'Legal' : 'Not Legal')
+
+// elif
+console.log(age >=33 && age <=50 ? 'Legal' : age < 10 ? 'Not Legal' : 'undifined')
+
+
+
